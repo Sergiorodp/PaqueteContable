c5e9be2954068ce26c4506291d979b9438ed215a
SergioRod <sergio.rod.perez@outlook.es>
2023-02-21 18:24:46 -0500

    Paquete contable primer commit

---
+++ pages/login/index.tsx
@@ -0,0 +1,8 @@
+import Login from 'features/login/MainPage'
+import type { NextPage } from 'next'
+
+const LoginPage : NextPage = () => {
+    return <Login/>
+} 
+
+export default LoginPage;
---
+++ shared/components/PrincipalLayout/types.tsx
@@ -0,0 +1,5 @@
+export type PrincipalLayoutProps = {
+    children : React.ReactNode
+    title : string
+    color? : string
+}
