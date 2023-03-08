0def8fe2e6f08ba0b706276f99a1a46c2acd6966
seandrod <sergio.rod.perez@outlook.es>
2023-03-08 16:50:12 -0500

    Agregado - apollo client y ejemplo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
+import { ApolloClient, HttpLink, gql, InMemoryCache} from '@apollo/client'
 import type { AppProps } from 'next/app'
+
+const uri : string = process.env.GRAPHQL_SERVER_URI ?? "http://localhost:8080/graphql" 
+
+const client = new ApolloClient({
+  uri : uri,
+  cache : new InMemoryCache()
+})
+
+client.query({
+  query : gql`
+    query {
+      getUsers {
+        name
+      }
+    }
+  `
+}).then(res => console.log(res.data))
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
