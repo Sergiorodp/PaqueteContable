d77cf20cbb607821d33c4d72fb1ab59e85b86c4f
SergioRod <sergio.rod.perez@outlook.es>
2023-03-11 03:05:04 -0500

    Agregado - separacion basica front topbar

--- shared/components/PrincipalTopBar/index.tsx
+++ shared/components/PrincipalTopBar/index.tsx
@@ -4,7 +4,15 @@ import styles from './PrincipalTopBar.module.css'
 export default function TopBar(){
     return(
         <div className={ styles.TopContainer}>  
-            
+            <div>
+                Info
+            </div>
+            <div>
+                Serach
+            </div>
+            <div>
+                profile
+            </div>
         </div>
         
     )
